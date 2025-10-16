c449b5cba41e34be8fe5d900c7ce51d4a909bd81
Mykhailo <90206262+Morphenoed@users.noreply.github.com>
2025-10-16 20:11:39 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO: Gib "Hello MEITREX" auf der Konsole aus
         System.out.println("Hello MEITREX");
+        
     }
 }
